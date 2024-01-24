0e1d49f074f7955b79d38e52169c3b4977db39b5
prestigeworldwidecr <prestigeworldwidecr@gmail.com>
2024-01-23 23:45:20 -0600

    makeArrayConsecutive2 ex7

--- almostIncreasingSequence/almostIncreasingSequence.js
+++ almostIncreasingSequence/almostIncreasingSequence.js
@@ -36,9 +36,129 @@ Return true if it is possible to remove one element from the array in order to g
 
 */
 
-function solution(sequence)
+function solution1(sequence)
 {
-    console.log("*");
+    cnt = 0;            // count out of sequence
+    currCnt = 0;        // count when currentMax overwhelmed
+    currentMax = -999999;
+
+    for (i = 0; i < sequence.length; i++)
+    {
+        if (sequence [i] >= sequence [i + 1])
+        {
+            cnt++;
+        }
+
+        else if (cnt > 1 || currCnt > 1)
+        {
+            return false;
+        }
+
+        else if (sequence [i + 1] <= currentMax)
+        {
+            // 
+        }
+
+        else
+        {
+            currentMax = sequence [i + 1];
+            currCnt++;
+        }
+
+        console.log("i: ", i, " sequence [i]: ", sequence [i], " sequence [i + 1]: ", sequence [i + 1], " cnt: ", cnt,  " currCnt: ",  currCnt, " currentMax: ", currentMax);
+    }
+
+    return true;
 }
 
-solution("");
+// different strategy using prevMax, currMax
+function solution2(sequence)
+{
+    prevMax = -999999;
+    currentMax = -999999;
+
+    for (i = 0; i < sequence.length; i++)
+    {
+        if (i == 0)
+        {
+            console.log("*1");
+            
+            if (sequence [i] < sequence [i + 1])
+            {
+                prevMax = sequence [i + 1];
+                currentMax = sequence [i + 1];
+            }
+
+            else
+            {
+                prevMax = sequence [i];
+                currentMax = sequence [i];
+            }
+        }
+
+        else if ( (sequence [i] >= sequence [i + 1]) &&
+                (sequence [i] >= currentMax) &&
+                (sequence [i] >= prevMax) )
+        {
+            console.log("*2");
+            return false;
+        }
+
+        else
+        {
+            console.log("*3");
+            prevMax = currentMax;
+            currentMax = sequence [i + 1];
+        }
+
+        console.log("i: ", i, " sequence [i]: ", sequence [i], " sequence [i + 1]: ", sequence [i + 1], " currentMax: ", currentMax, " prevMax: ", prevMax);
+
+        /*
+
+        if (sequence [i] >= sequence [i + 1])
+        {
+            cnt++;
+        }
+
+        
+        else if (sequence [i + 1] <= currentMax)
+        {
+            // 
+        }
+
+        else
+        {
+            currentMax = sequence [i + 1];
+        }
+
+        */
+
+    }
+
+    return true;
+}
+
+s1 = [1, 3, 2, 1];                          // false
+s2 = [1, 3, 2];                             // true
+s3 = [1, 2, 1, 2];                          // false
+s4 = [1, 2, 3, 4, 5, 3, 5, 6];              // false
+s5 = [40, 50, 60, 10, 20, 30]               // false
+s6 = [1, 2, 5, 3, 5];                       // 
+s7 = [1, 2, 3, 4, 99, 5, 6];                // 
+s8 = [123, -17, -5, 1, 2, 3, 12, 43, 45]    // 
+
+console.log(solution1(s3));
+
+/*
+
+********
+BONEYARD
+********
+
+// [1, 3, 2, 1]     // false
+// [1, 3, 2]        // true
+
+// one element outta order, cnt++
+// > 1, outta order return false
+
+*/
