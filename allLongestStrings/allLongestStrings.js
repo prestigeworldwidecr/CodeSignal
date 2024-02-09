96da67952d424bdd3e59ee5dc4c2cd5e5d441198
prestigeworldwidecr <prestigeworldwidecr@gmail.com>
2024-02-09 07:11:23 -0500

    allLongestStrings ex9

--- allLongestStrings/allLongestStrings.js
+++ allLongestStrings/allLongestStrings.js
@@ -29,3 +29,75 @@ Array of the longest strings, stored in the same order as in the inputArray.
 
 */
 
+function maxLengthString(inputArray)
+{
+    var i = 0;
+    var tmp = null;
+    var max = -999999;
+    
+    for (i; i < inputArray.length; i++)
+    {
+        tmp = inputArray[i].length;
+
+        if (tmp > max)
+        {
+            max = tmp;
+        }
+
+        else
+        {
+            // 
+        }
+    }
+
+    return max;
+
+}
+
+function solution(inputArray) 
+{
+    var i = 0;
+    var mls = maxLengthString(inputArray);
+    var result = [];
+
+    for (i; i < inputArray.length; i++)
+    {
+        // console.log(inputArray[i]);
+
+        if (inputArray[i].length == mls)
+        {
+            result.push(inputArray[i]);
+        }
+
+        else
+        {
+            //
+        }
+
+    }
+
+    return result;
+}
+
+// console.log(solution(["aba", "aa", "ad", "vcd", "aba"]));
+console.log(solution(["aba", "aa", "ad", "vcd", "aba"]));
+
+
+/*
+
+********
+BONEYARD
+********
+
+// var i = inputArray.length
+
+    // console.log(i);
+    // console.log(inputArray[0].length);
+    // console.log(inputArray[0].length);
+    // console.log(Math.max(inputArray));
+
+Math.max
+.length
+
+
+*/
