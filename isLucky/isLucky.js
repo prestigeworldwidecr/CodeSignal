d81266486972916ad270d7d1b3102a6e6df25ba7
prestigeworldwidecr <prestigeworldwidecr@gmail.com>
2024-02-16 17:55:22 -0600

    isLucky ex11

--- isLucky/isLucky.js
+++ isLucky/isLucky.js
@@ -35,52 +35,68 @@ true if n is a lucky ticket number, false otherwise.
 
 function isLucky2digit(n)
 {
-    var tmp = 0;
+    var tmp = null;
+
+    tmp = [parseInt(n[0]), parseInt(n[1])];
+
+    return (tmp [0] == tmp [1]);
 }
 
 function isLucky4digit(n)
 {
-    
+    var tmp = null;
+
+    // console.log(tmp);
+
+    tmp = [parseInt(n[0]), parseInt(n[1]), parseInt(n[2]), parseInt(n[3])];
+
+    return (tmp [0] + tmp [1]) == (tmp [2] + tmp [3]);
 }
 
-function isLucky5digit(n)
+function isLucky6digit(n)
 {
-    
+    var tmp = null;
+
+    tmp = [parseInt(n[0]), parseInt(n[1]), parseInt(n[2]), parseInt(n[3]), parseInt(n[4]), parseInt(n[5])];
+
+    return (tmp [0] + tmp [1] + tmp [2]) == (tmp [3] + tmp [4] + tmp [5]);
 }
 
 function solution(n) 
 {
+    var result = false;
     var tmp = "";
 
     tmp = n.toString();
 
-    // console.log(tmp);
-
     if (tmp.length == 2)
     {
-        isLucky2digit(n);
+        result = isLucky2digit(tmp);
     }
 
     else if (tmp.length == 4)
     {
-        isLucky4digit(n);
+        result = isLucky4digit(tmp);
     }
 
     else if (tmp.length == 6)
     {
-        isLucky6digit(n);
+        result = isLucky6digit(tmp);
     }
 
     else
     {
         return false;
     }
+
+    // console.log(result);
 }
 
 n1 = 1230;
 n2 = 239017;
+n3 = 31;
 
-solution(n1);
+solution(n2);
 
 /*
 
