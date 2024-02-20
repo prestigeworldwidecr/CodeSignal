1c65feda9d3d15f60d9ac1d210c9ec1b3e55e80e
prestigeworldwidecr <prestigeworldwidecr@gmail.com>
2024-02-19 23:14:48 -0600

    arrayMaximalAdjacentDifference ex20

--- arrayMaximalAdjacentDifference/arrayMaximalAdjacentDifference.js
+++ arrayMaximalAdjacentDifference/arrayMaximalAdjacentDifference.js
@@ -27,6 +27,33 @@ The maximal absolute difference.
 
 function solution(inputArray) 
 {
+    var i = 0;
+    var pair = [inputArray[0], inputArray[1]];
+    var min = Math.min(...pair);
+    var max = Math.max(...pair);
+    var tmp = -999999;
+    var mad = -999999;   // Maximal Adjacent Difference
+    
+    for (i; i < inputArray.length - 1; i++)
+    {
+        pair = [inputArray[i], inputArray[i + 1]];
+        min = Math.min(...pair);
+        max = Math.max(...pair);
+        tmp = max - min;
+
+        if (tmp > mad)
+        {
+            mad = tmp;
+        }
+
+        else
+        {
+            // 
+        }
+
+    }
+
+    return mad;
 
 }
 
@@ -35,5 +62,12 @@ function solution(inputArray)
 ********
 BONEYARD
 ********
+
+// console.log("pair: ", pair, " min: ", min, " max: ", max);
+    // console.log("pair: ", pair, " min: ", min, " max: ", max, " tmp: ", tmp);
+    // console.log(mad);
+
+inputArray = [2, 4, 1, 0];  // 
+solution(inputArray);
 
 */
