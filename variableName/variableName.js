b2fe76addfa8f97e2c6d645adcc12c448faf0861
prestigeworldwidecr <prestigeworldwidecr@gmail.com>
2024-02-28 19:42:40 -0600

    variableName ex27

---
+++ variableName/variableName.js
@@ -0,0 +1,71 @@
+/*
+
+Correct variable names consist only of English letters, digits and underscores and they can't start with a digit.
+
+Check if the given string is a correct variable name.
+
+Example
+
+For name = "var_1__Int", the output should be
+solution(name) = true;
+
+For name = "qq-q", the output should be
+solution(name) = false;
+
+For name = "2w2", the output should be
+solution(name) = false.
+
+Input/Output
+
+[execution time limit] 4 seconds (js)
+
+[memory limit] 1 GB
+
+[input] string name
+
+Guaranteed constraints:
+1 ≤ name.length ≤ 10.
+
+[output] boolean
+
+true if name is a correct variable name, false otherwise.
+
+*/
+
+function allValidCharacters(name)
+{
+    let result = false;
+    
+    for (let i = 0; i < name.length; i++)
+    {
+        if ()
+        {
+
+        }
+
+        else
+        {
+            
+        }
+    }
+}
+
+function solution(name) 
+{
+
+}
+
+
+name1 = "var_1__Int" // the output should be solution(name) = true;
+name2 = "qq-q"   // the output should be solution(name) = false;
+name3 = "2w2"    //the output should be solution(name) = false.
+
+console.log(name1.charCodeAt(1));
+
+/*
+
+********
+BONEYARD
+********
+
+*/
