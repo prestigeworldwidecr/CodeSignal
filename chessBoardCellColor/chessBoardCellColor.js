ac160cce046817079b8d04706e57f2afb0d0357a
prestigeworldwidecr <prestigeworldwidecr@gmail.com>
2024-02-28 22:06:28 -0600

    chessBoardCellColor ex29

--- chessBoardCellColor/chessBoardCellColor.js
+++ chessBoardCellColor/chessBoardCellColor.js
@@ -7,12 +7,8 @@ Example
 For cell1 = "A1" and cell2 = "C3", the output should be
 solution(cell1, cell2) = true.
 
-
-
 For cell1 = "A1" and cell2 = "H3", the output should be
 solution(cell1, cell2) = false.
-
-
 
 Input/Output
 
@@ -40,7 +36,44 @@ true if both cells have the same color, false otherwise.
 
 */
 
+function chessBoardCellColor(cell1, cell2)
+{
+    let val1 = cell1.charCodeAt(0) + cell1.charCodeAt(1);
+    let val2 = cell2.charCodeAt(0) + cell2.charCodeAt(1);
+
+    console.log("val1: ", val1, " val2: ", val2);
+}
+
 function solution(cell1, cell2) 
 {
 
 }
+
+let cell1 = "A1";
+let cell2 = "C3";    // the output should be solution(cell1, cell2) = true
+
+let cell1 = "A1"; 
+let cell2 = "H3";    // , the output should be solution(cell1, cell2) = false
+
+/*
+
+********
+BONEYARD
+********
+
+STRAT
+-----
+
+ASCII 65-72
+ABCDEFGH
+
+12345678
+ASCII 49-57
+
+A1  11  2
+C3  33  6
+
+A1  11  2
+H3  83  11
+
+*/
