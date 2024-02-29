cdeb22b5948ae52dad05fbe0568f2b6086dce5da
prestigeworldwidecr <prestigeworldwidecr@gmail.com>
2024-02-28 20:36:07 -0600

    alphabeticShift ex28

--- variableName/variableName.js
+++ variableName/variableName.js
@@ -34,33 +34,58 @@ true if name is a correct variable name, false otherwise.
 
 function allValidCharacters(name)
 {
-    let result = false;
+    let tmp = -1;
+    let result = "";
     
     for (let i = 0; i < name.length; i++)
     {
-        if ()
-        {
+        tmp = name.charCodeAt(i);
 
+        if (48 <= tmp && tmp <= 57 ||   // 0-9
+            65 <= tmp && tmp <= 90 ||   // A-Z
+            97 <= tmp && tmp <= 122 ||  // a-z
+            tmp == 95)  // _
+        {
+            result = true;
         }
 
         else
         {
-            
+            return false;
         }
     }
+
+    return result;
 }
 
 function solution(name) 
 {
+    let result = "";
+    tmp = name.charCodeAt(0);
 
+    if (65 <= tmp && tmp <= 90 ||   // A-Z
+        97 <= tmp && tmp <= 122 ||  // a-z
+        tmp == 95)  // _
+    {
+        result = allValidCharacters(name);
+    }
+
+    else
+    {
+        result = false;
+    }
+
+    return result;
 }
 
 
-name1 = "var_1__Int" // the output should be solution(name) = true;
-name2 = "qq-q"   // the output should be solution(name) = false;
-name3 = "2w2"    //the output should be solution(name) = false.
+name1 = "var_1__Int"; // the output should be solution(name) = true;
+name2 = "qq-q";   // the output should be solution(name) = false;
+name3 = "2w2";    //the output should be solution(name) = false.
 
-console.log(name1.charCodeAt(1));
+// console.log(name1.charCodeAt(1));
+
+console.log(solution(name3));
 
 /*
 
