b5be47fc1a1cb12dcdd9d29e33e368941aab35a0
prestigeworldwidecr <prestigeworldwidecr@gmail.com>
2024-03-02 07:33:46 -0600

    extractEachKth ex34

---
+++ extractEachKth/extractEachKth.js
@@ -0,0 +1,50 @@
+/*
+
+Given array of integers, remove each kth element from it.
+
+Example
+
+For inputArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10] and k = 3, the output should be
+solution(inputArray, k) = [1, 2, 4, 5, 7, 8, 10].
+
+Input/Output
+
+[execution time limit] 4 seconds (js)
+
+[memory limit] 1 GB
+
+[input] array.integer inputArray
+
+Guaranteed constraints:
+5 ≤ inputArray.length ≤ 15,
+-20 ≤ inputArray[i] ≤ 20.
+
+[input] integer k
+
+Guaranteed constraints:
+1 ≤ k ≤ 10.
+
+[output] array.integer
+
+inputArray without elements k - 1, 2k - 1, 3k - 1 etc.
+
+*/
+
+function solution(inputArray, k) 
+{
+
+    const myArray = [1, 2, 3, 4, 5];
+
+const index = myArray.indexOf(2);
+
+const x = myArray.splice(index, 1);
+
+}
+
+/*
+
+********
+BONEYARD
+********
+
+*/
