2a8f4c40b4d35d60d419885a842350539d9b66d6
prestigeworldwidecr <prestigeworldwidecr@gmail.com>
2024-03-15 08:48:31 -0600

    longestDigitsPrefix ex40

--- longestDigitsPrefix/longestDigitsPrefix.js
+++ longestDigitsPrefix/longestDigitsPrefix.js
@@ -25,34 +25,43 @@ Guaranteed constraints:
 function gotDigits(inputString)
 {
     var tmp = inputString.split('');
+    // var result = 0;
+    var result = "";
     
-    console.log(tmp);
+    // console.log(tmp);
 
     for (var i = 0; i < tmp.length; i++)
     {
-        console.log("!");
+        // console.log(tmp[i]);
+
+        if (Number(tmp[i]) || tmp[i] == "0")
+        {
+            // result++;
+            result = result + tmp[i];
+        }
+
+        else
+        {
+            i = Number.MAX_SAFE_INTEGER;
+        }
+
     }
 
-
-/*
-
-tmp1 = tmp1.toString();
-if (0 <= Number(tmp) && Number(tmp) < 256)
-tmp1 = tmp.split('').reverse();
-
-*/
+    // console.log(result);
+    return result;
 
 }
 
 function solution(inputString) 
 {
-
+    return gotDigits(inputString);
 }
 
 
 var inputString = "123aa1";  // solution(inputString) = "123"
+var inputString = "0123456789"; // "0123456789"
 
-gotDigits(inputString);
+// gotDigits(inputString);
 
 /*
 
@@ -60,6 +69,8 @@ gotDigits(inputString);
 BONEYARD
 ********
 
-
+tmp1 = tmp1.toString();
+if (0 <= Number(tmp) && Number(tmp) < 256)
+tmp1 = tmp.split('').reverse();
 
 */
