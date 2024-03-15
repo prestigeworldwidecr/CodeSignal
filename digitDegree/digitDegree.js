ae38bf49af30ad86861e903218c1471dad4728a3
prestigeworldwidecr <prestigeworldwidecr@gmail.com>
2024-03-15 08:54:13 -0600

    digitDegree ex41

---
+++ digitDegree/digitDegree.js
@@ -0,0 +1,43 @@
+/*
+
+Let's define digit degree of some positive integer as the number of times we need to replace this number with the sum of its digits until we get to a one digit number.
+
+Given an integer, find its digit degree.
+
+Example
+
+For n = 5, the output should be
+solution(n) = 0;
+For n = 100, the output should be
+solution(n) = 1.
+1 + 0 + 0 = 1.
+For n = 91, the output should be
+solution(n) = 2.
+9 + 1 = 10 -> 1 + 0 = 1.
+Input/Output
+
+[execution time limit] 4 seconds (js)
+
+[memory limit] 1 GB
+
+[input] integer n
+
+Guaranteed constraints:
+5 ≤ n ≤ 109.
+
+[output] integer
+
+*/
+
+function solution(n) 
+{
+
+}
+
+/*
+
+********
+BONEYARD
+********
+
+*/
