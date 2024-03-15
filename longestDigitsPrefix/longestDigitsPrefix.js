cb2ead9bcfc0252fdc9e740b274431ac5ba603a2
prestigeworldwidecr <prestigeworldwidecr@gmail.com>
2024-03-15 08:21:37 -0600

    longestDigitsPrefix ex40

--- longestDigitsPrefix/longestDigitsPrefix.js
+++ longestDigitsPrefix/longestDigitsPrefix.js
@@ -22,10 +22,37 @@ Guaranteed constraints:
 
 */
 
+function gotDigits(inputString)
+{
+    var tmp = inputString.split('');
+    
+    console.log(tmp);
+
+    for (var i = 0; i < tmp.length; i++)
+    {
+        console.log("!");
+    }
+
+
+/*
+
+tmp1 = tmp1.toString();
+if (0 <= Number(tmp) && Number(tmp) < 256)
+tmp1 = tmp.split('').reverse();
+
+*/
+
+}
+
 function solution(inputString) 
 {
 
 }
+
+
+var inputString = "123aa1";  // solution(inputString) = "123"
+
+gotDigits(inputString);
 
 /*
 
