60a61e530f355c4c7fa920232e206f988fc64d21
prestigeworldwidecr <prestigeworldwidecr@gmail.com>
2024-03-15 08:11:31 -0600

    longestDigitsPrefix ex40

---
+++ longestDigitsPrefix/longestDigitsPrefix.js
@@ -0,0 +1,38 @@
+/*
+
+Given a string, output its longest prefix which contains only digits.
+
+Example
+
+For inputString = "123aa1", the output should be
+solution(inputString) = "123".
+
+Input/Output
+
+[execution time limit] 4 seconds (js)
+
+[memory limit] 1 GB
+
+[input] string inputString
+
+Guaranteed constraints:
+3 ≤ inputString.length ≤ 100.
+
+[output] string
+
+*/
+
+function solution(inputString) 
+{
+
+}
+
+/*
+
+********
+BONEYARD
+********
+
+
+
+*/
