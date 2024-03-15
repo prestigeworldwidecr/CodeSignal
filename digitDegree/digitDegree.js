7c9d1adfdd65719b75808b8b23b1a650a7ae3ab5
prestigeworldwidecr <prestigeworldwidecr@gmail.com>
2024-03-15 17:30:32 -0600

    digitDegree ex41

--- digitDegree/digitDegree.js
+++ digitDegree/digitDegree.js
@@ -29,22 +29,47 @@ Guaranteed constraints:
 
 */
 
+function digitDegree(n, cnt)
+{
+    tmp = n.toString();
+    
+    if (tmp.length == 1)
+    {
+        return 0 + cnt;
+    }
+
+    else
+    {
+        return 1 + digitDegree(sumIndividualDigit(n), 0);
+    }
+
+}
+
 function sumIndividualDigit(n)
 {
+    var tmp = n.toString();
+    var sum = 0;
 
+    for (var i = 0; i < tmp.length; i++)
+    {
+        sum = sum + Number(tmp[i]);
+    }
+
+    return sum;
 }
 
 function solution(n) 
 {
-
+    return digitDegree(n, 0);
 }
 
 
 var n = 5;  // the output should be solution(n) = 0;
-var n = 100 // the output should be solution(n) = 1.
-var n = 91  // the output should be solution(n) = 2.
+// var n = 100 // the output should be solution(n) = 1.
+// var n = 91  // the output should be solution(n) = 2.
 
-sumIndividualDigit(n);
+// console.log(sumIndividualDigit(n));
+
 
 /*
 
