3e43679ad0938f6bd220cf73424b4881ae88f77d
prestigeworldwidecr <prestigeworldwidecr@gmail.com>
2024-03-16 22:45:26 -0600

    isBeautifulString ex43

--- isBeautifulString/isBeautifulString.js
+++ isBeautifulString/isBeautifulString.js
@@ -37,6 +37,39 @@ Return true if the string is beautiful, false otherwise.
 
 */
 
+function alphabetCount()
+{
+    var cnt = [ ['a', 0],
+                ['b', 0],
+                ['c', 0],
+                ['d', 0],
+                ['e', 0],
+                ['f', 0],
+                ['g', 0],
+                ['h', 0],
+                ['i', 0],
+                ['j', 0],
+                ['k', 0],
+                ['l', 0],
+                ['m', 0],
+                ['n', 0],
+                ['o', 0],
+                ['p', 0],
+                ['q', 0],
+                ['r', 0],
+                ['s', 0],
+                ['t', 0],
+                ['u', 0],
+                ['v', 0],
+                ['w', 0],
+                ['x', 0],
+                ['y', 0],
+                ['z', 0] ];
+
+    return cnt;
+
+}
+
 function alphabetizeString(s)
 {
     var i = 0;
@@ -47,38 +80,6 @@ function alphabetizeString(s)
     tmp = tmp.join("");
 
     return tmp;
-}
-
-function removeDuplicates(s)
-{
-    var tmp = [...new Set(s)].join("");
-
-    return tmp;
-}
-
-/*
-
-    "aabcc"
-    x   y
-    a   0
-    b   0
-    c   0
-
-*/
-function string2DSansDuplicate(s)
-{
-    var i = 0;
-    var tmp = [];
-
-    s = removeDuplicates(s);
-
-    for (i; i < s.length; i++)
-    {
-        tmp.push([s[i], 0]);
-    }
-
-    return tmp;
-
 }
 
 // input: alphabetized string, 2D array containing corresponding characters non-dup
@@ -113,38 +114,49 @@ function countCharacterInstances(s, s2d)
     return s2d;
 }
 
-function compare2DStrings(s1, s2)
+function isBeautifulString(alpha2D)
 {
-    var i = 0;
-    var j = 0;
-    var result = 0;
+    var prev = alpha2D[0][1];
+    var cur = alpha2D[0][1];
+    var result = false;
 
-    for (i; i < s1.length; i++)
+    // console.log(alpha2D[0][1]);
+    
+    for (var i = 0; i < alpha2D.length; i++)
     {
-        j = 0;
-
-        for (j; j < s2.length; j++)
+        cur = alpha2D[i][1];
+        console.log("cur: ", cur, " prev: ", prev);
+        
+        if (cur > prev)
         {
-            if (s1[i][0] == s2[j][0])
-            {
-                result = result + Math.min(s1[i][1], s2[j][1]);
-                j = s2.length; // break
-            }
-
-            else
-            {
-                //
-            }
+            return false;
         }
-    }
 
+        else
+        {
+            prev = cur;
+            result = true;
+        }
+        
+    }
+    
     return result;
+
 }
 
 function solution(inputString) 
 {
-
+    return isBeautifulString(countCharacterInstances(alphabetizeString(inputString), alphabetCount()));
 }
+
+inputString = "bbbaacdafe"  // the output should be solution(inputString) = true.
+// This string contains 3 as, 3 bs, 1 c, 1 d, 1 e, and 1 f (and 0 of every other letter), so since there aren't any letters that appear more frequently than the previous letter, this string qualifies as beautiful.
+// inputString = "aabbb"       // the output should be solution(inputString) = false.
+// Since there are more bs than as, this string is not beautiful.
+// inputString = "bbc"         // the output should be solution(inputString) = false.
+
+// console.log(alphabetCount());
+// console.log(isBeautifulString(countCharacterInstances(alphabetizeString(inputString), alphabetCount())));
 
 /*
 
