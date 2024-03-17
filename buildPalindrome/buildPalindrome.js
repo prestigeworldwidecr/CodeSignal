79d837b8fbd99ae01086818600df6d79775f4420
prestigeworldwidecr <prestigeworldwidecr@gmail.com>
2024-03-17 11:08:45 -0600

    buildPalindrome ex45

---
+++ buildPalindrome/buildPalindrome.js
@@ -0,0 +1,38 @@
+/*
+
+Given a string, find the shortest possible string which can be achieved by adding characters to the end of initial string to make it a palindrome.
+
+Example
+
+For st = "abcdc", the output should be
+solution(st) = "abcdcba".
+
+Input/Output
+
+[execution time limit] 4 seconds (js)
+
+[memory limit] 1 GB
+
+[input] string st
+
+A string consisting of lowercase English letters.
+
+Guaranteed constraints:
+3 ≤ st.length ≤ 10.
+
+[output] string
+
+*/
+
+function solution(st) 
+{
+
+}
+
+/*
+
+********
+BONEYARD
+********
+
+*/
