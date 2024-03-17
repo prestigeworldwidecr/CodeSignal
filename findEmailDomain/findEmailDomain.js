a9852dba00eaf25f214c372ee92babb4975f6af2
prestigeworldwidecr <prestigeworldwidecr@gmail.com>
2024-03-17 11:07:39 -0600

    findEmailDomain ex44

--- findEmailDomain/findEmailDomain.js
+++ findEmailDomain/findEmailDomain.js
@@ -29,21 +29,61 @@ Guaranteed constraints:
 
 function findEmailDomain(s)
 {
+    var tmp = s.split("@");
+
+    result = tmp[tmp.length - 1];
+    result = result.replace(/"/g, "");
+
+    if ( (65 > result.charCodeAt(0) && result.charCodeAt(0) > 90) ||  // A-Z
+         (97 > result.charCodeAt(0) && result.charCodeAt(0) > 122) )  // a-z
+    {
+        result = result.substring(1, result.length);
+    }
+
+    else
+    {
+        // 
+    }
     
+    return result;
 }
 
 function solution(address) 
 {
-
+    return findEmailDomain(address);
 }
 
-var address = "prettyandsimple@example.com"     // the output should be solution(address) = "example.com";
-var address = "fully-qualified-domain@codesignal.com"   // the output should be solution(address) = "codesignal.com".
+// var address = "prettyandsimple@example.com"     // the output should be solution(address) = "example.com";
+// var address = "fully-qualified-domain@codesignal.com"   // the output should be solution(address) = "codesignal.com".
+var address = "\"very.unusual.@.unusual.com\"@usual.com"    // "usual.com"
+
+console.log(findEmailDomain(address));
 
 /*
 
 ********
 BONEYARD
 ********
+
+function breakStringAt(s)
+{
+    tmp = s.split("@");
+
+    console.log(tmp.length, " a ver: ", tmp[tmp.length -1]);
+}
+
+// breakStringAt(address);
+// console.log("tmp.length: ", tmp.length, " tmp[0]: ", tmp[0], " tmp[1]: ", tmp[1], " tmp[tmp.length - 1]: ", tmp[tmp.length - 1]);
+// console.log(s);
+
+// var result = [tmp.length - 2].toString();
+
+    // console.log("tmp: ", tmp, " result: ", result);
+    // console.log("tmp: ", tmp, " result: ", result);
+    // result = result.replace(/"/g, "");
+    // console.log(result);
+        // console.log("!");
+    // console.log(s.split("@")[1]);
+    // console.log("tmp: ", tmp, " result: ", result);
 
 */
