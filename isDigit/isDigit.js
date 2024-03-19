e8954c98214410af2d605ef8cf06cf0f8b174f8d
prestigeworldwidecr <prestigeworldwidecr@gmail.com>
2024-03-19 09:53:23 -0600

    isDigit ex48

--- isDigit/isDigit.js
+++ isDigit/isDigit.js
@@ -29,8 +29,20 @@ true if symbol is a digit, false otherwise.
 
 function solution(symbol) 
 {
-    return 48 <= symbol && symbol <= 57;
+    // console.log(symbol);
+    var s = "";
+    var tmp = 0;
+    
+    s = symbol;
+    s = symbol.toString();
+    tmp = s.charCodeAt(0);
+    
+    console.log(tmp);
+
+    return 48 <= tmp && tmp <= 57;
 }
+
+solution(0);
 
 /*
 
