d8d3baddcb9c4d4a3dad45d605cc8826c5c03dff
prestigeworldwidecr <prestigeworldwidecr@gmail.com>
2024-03-22 18:04:20 -0600

    sudoku ex60

--- sudoku/sudoku.js
+++ sudoku/sudoku.js
@@ -58,10 +58,60 @@ true if the given grid represents a correct solution to Sudoku, false otherwise.
 
 */
 
+function scanSudoku(grid)
+{
+    // console.log("!");
+
+    let tmp =       [["x", "x", "x"],
+                    ["x", "x", "x"],
+                    ["x", "x", "x"]];
+
+    for (let i = 0; i < grid.length; i++)
+    {
+        var s = "";
+        
+        for (let j = 0; j < grid[0].length; j++)
+        {
+            // console.log(grid[i][j]);
+            s = s + grid[i][j];
+        }
+
+        console.log(s);
+
+    }
+
+    console.log(tmp);
+}
+
 function solution(grid) 
 {
 
 }
+
+let grid =  [[1, 3, 2, 5, 4, 6, 9, 8, 7],
+            [4, 6, 5, 8, 7, 9, 3, 2, 1],
+            [7, 9, 8, 2, 1, 3, 6, 5, 4],
+            [9, 2, 1, 4, 3, 5, 8, 7, 6],
+            [3, 5, 4, 7, 6, 8, 2, 1, 9],
+            [6, 8, 7, 1, 9, 2, 5, 4, 3],
+            [5, 7, 6, 9, 8, 1, 4, 3, 2],
+            [2, 4, 3, 6, 5, 7, 1, 9, 8],
+            [8, 1, 9, 3, 2, 4, 7, 6, 5]]
+
+        // the output should be solution(grid) = true
+
+grid = [[1, 3, 4, 2, 5, 6, 9, 8, 7],
+        [4, 6, 8, 5, 7, 9, 3, 2, 1],
+        [7, 9, 2, 8, 1, 3, 6, 5, 4],
+        [9, 2, 3, 1, 4, 5, 8, 7, 6],
+        [3, 5, 7, 4, 6, 8, 2, 1, 9],
+        [6, 8, 1, 7, 9, 2, 5, 4, 3],
+        [5, 7, 6, 9, 8, 1, 4, 3, 2],
+        [2, 4, 5, 6, 3, 7, 1, 9, 8],
+        [8, 1, 9, 3, 2, 4, 7, 6, 5]]
+// the output should be solution(grid) = false
+
+scanSudoku(grid);
 
 /*
 
