bea08ebe20155e411de33b32cab01b9cb81f53f2
prestigeworldwidecr <prestigeworldwidecr@gmail.com>
2024-04-01 15:09:20 -0600

    sudoku ex60

--- sudoku/sudoku.js
+++ sudoku/sudoku.js
@@ -58,33 +58,64 @@ true if the given grid represents a correct solution to Sudoku, false otherwise.
 
 */
 
-function scanSudoku(grid)
+function scanSudoku(grid, startX, endX, startY, endY)
 {
-    // console.log("!");
+    let a = [];
 
-    let tmp =       [["x", "x", "x"],
-                    ["x", "x", "x"],
-                    ["x", "x", "x"]];
-
-    for (let i = 0; i < grid.length; i++)
+    for (let i = startX; i < endX; i++)
     {
-        var s = "";
         
-        for (let j = 0; j < grid[0].length; j++)
+        for (let j = startY; j < endY; j++)
         {
-            // console.log(grid[i][j]);
-            s = s + grid[i][j];
+            a.push(grid [i][j]);
         }
-
-        console.log(s);
 
     }
 
-    console.log(tmp);
+    return a;
+
+}
+
+function isSudokuCompliant(a)
+{
+    let result = false;
+    let j = 1;
+
+    a.sort();
+
+    for (let i = 0; i < a.length; i++)
+    {
+        if (a[i] == j)
+        {
+            result = true;
+            j++;
+        }
+
+        else
+        {
+            return false;
+        }
+    }
+
+    return result;
+
 }
 
 function solution(grid) 
 {
+    let result1 = isSudokuCompliant(scanSudoku(grid, 0, 3, 0, 3));
+    let result2 = isSudokuCompliant(scanSudoku(grid, 0, 3, 3, 6));
+    let result3 = isSudokuCompliant(scanSudoku(grid, 0, 3, 6, 9));
+    let result4 = isSudokuCompliant(scanSudoku(grid, 3, 6, 0, 3));
+    let result5 = isSudokuCompliant(scanSudoku(grid, 3, 6, 3, 6));
+    let result6 = isSudokuCompliant(scanSudoku(grid, 3, 6, 6, 9));
+    let result7 = isSudokuCompliant(scanSudoku(grid, 6, 9, 0, 3));
+    let result8 = isSudokuCompliant(scanSudoku(grid, 6, 9, 3, 6));
+    let result9 = isSudokuCompliant(scanSudoku(grid, 6, 9, 6, 9));
+
+    // console.log(result1 && result2 && result3 && result4 && result5 && result6 && result7 && result8 && result9);
+    
+    return (result1 && result2 && result3 && result4 && result5 && result6 && result7 && result8 && result9);
 
 }
 
@@ -108,15 +139,85 @@ grid = [[1, 3, 4, 2, 5, 6, 9, 8, 7],
         [6, 8, 1, 7, 9, 2, 5, 4, 3],
         [5, 7, 6, 9, 8, 1, 4, 3, 2],
         [2, 4, 5, 6, 3, 7, 1, 9, 8],
-        [8, 1, 9, 3, 2, 4, 7, 6, 5]]
-// the output should be solution(grid) = false
+        [8, 1, 9, 3, 2, 4, 7, 6, 5]];
 
-scanSudoku(grid);
+        // the output should be solution(grid) = false
+
+// console.log(isSudokuCompliant(scanSudoku(grid, 0, 3, 0, 3)));
+solution(grid);
 
 /*
 
 ********
 BONEYARD
 ********
+
+// s = s + grid [i][j];
+// console.log(a[i])
+// console.log(a);
+    // console.log(s);
+        // console.log(s.charAt(i));
+
+function printMatrix(matrix)
+{
+    let tmp = "";
+
+    for (let i = 0; i < matrix.length; i++)
+    {
+        tmp = "";
+
+        for (let j = 0; j < matrix[0].length; j++)
+        {
+            tmp = tmp + matrix[i][j] + "\t";
+        }
+
+        console.log(tmp);
+
+    }
+
+}
+
+// s = "";
+        
+        // start xy, end xy
+        
+
+    let tmp =       [["x", "x", "x"],
+                    ["x", "x", "x"],
+                    ["x", "x", "x"]];
+
+    // console.log(tmp);
+    // printMatrix(tmp);
+    // console.log(tmp.includes("1"));
+// printMatrix(grid);
+
+    tmp [0][0] = grid[0][0];
+    tmp [0][1] = grid[0][1];
+    tmp [0][2] = grid[0][2];
+    tmp [1][0] = grid[1][0];
+    tmp [1][1] = grid[1][1];
+    tmp [1][2] = grid[1][2];
+    tmp [2][0] = grid[2][0];
+    tmp [2][1] = grid[2][1];
+    tmp [2][2] = grid[2][2];
+
+for (let i = 0; i < grid.length; i++)
+    {
+        let s = "";
+        
+        for (let j = 0; j < grid[0].length; j++)
+        {
+            s = s + grid[i][j];
+        }
+
+        
+    }
+
+// console.log("!");
+// console.log(s);
+    // console.log(tmp);
+            // console.log(grid[i][j]);
+// console.log("x: ", i, " y: ", j, grid [i][j]);
+        // console.log(s);
 
 */
