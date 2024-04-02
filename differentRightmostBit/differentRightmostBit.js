90a5646b5bab6edcbef56c9c92b9b999e7a5d6a2
prestigeworldwidecr <prestigeworldwidecr@gmail.com>
2024-04-02 14:34:08 -0600

    differentRightmostBit ex23

---
+++ differentRightmostBit/differentRightmostBit.js
@@ -0,0 +1,64 @@
+/*
+
+Implement the missing code, denoted by ellipses. You may not modify the pre-existing code.
+You're given two integers, n and m. Find position of the rightmost bit in which they differ in their binary representations (it is guaranteed that such a bit exists), counting from right to left.
+
+Return the value of 2position_of_the_found_bit (0-based).
+
+Example
+
+For n = 11 and m = 13, the output should be
+solution(n, m) = 2.
+
+1110 = 10112, 1310 = 11012, the rightmost bit in which they differ is the bit at position 1 (0-based) from the right in the binary representations.
+So the answer is 21 = 2.
+
+For n = 7 and m = 23, the output should be
+solution(n, m) = 16.
+
+710 = 1112, 2310 = 101112, i.e.
+
+00111
+10111
+So the answer is 24 = 16.
+
+Input/Output
+
+[execution time limit] 4 seconds (js)
+
+[memory limit] 1 GB
+
+[input] integer n
+
+Guaranteed constraints:
+0 ≤ n ≤ 2^30.
+
+[input] integer m
+
+Guaranteed constraints:
+0 ≤ m ≤ 2^30,
+n ≠ m.
+
+[output] integer
+
+*/
+
+function 
+
+function solution(n, m) 
+{
+  // return ...;
+}
+
+let n = 11;
+let m = 13;     // 2
+n = 7;
+m = 23;     // the output should be solution(n, m) = 16.
+
+/*
+
+********
+BONEYARD
+********
+
+*/
