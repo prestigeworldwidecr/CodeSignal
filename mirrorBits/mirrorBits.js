a685eab8bb7263cb6326d71a175eb39358b2189b
prestigeworldwidecr <prestigeworldwidecr@gmail.com>
2024-04-02 11:55:22 -0600

    mirrorBits ex20

---
+++ mirrorBits/mirrorBits.js
@@ -0,0 +1,41 @@
+/*
+
+Reverse the order of the bits in a given integer.
+
+Example
+
+For a = 97, the output should be
+solution(a) = 67.
+
+97 equals to 1100001 in binary, which is 1000011 after mirroring, and that is 67 in base 10.
+
+For a = 8, the output should be
+solution(a) = 1.
+
+Input/Output
+
+[execution time limit] 4 seconds (js)
+
+[memory limit] 1 GB
+
+[input] integer a
+
+Guaranteed constraints:
+5 ≤ a ≤ 105.
+
+[output] integer
+
+*/
+
+function solution(a) 
+{
+
+}
+
+/*
+
+********
+BONEYARD
+********
+
+*/
