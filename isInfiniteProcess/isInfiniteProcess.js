1e83b63f4f51853dd8198f3b7d7d87df8d7d75f6
prestigeworldwidecr <prestigeworldwidecr@gmail.com>
2024-04-01 22:46:24 -0600

    isInfiniteProcess ex12

--- isInfiniteProcess/isInfiniteProcess.js
+++ isInfiniteProcess/isInfiniteProcess.js
@@ -35,15 +35,109 @@ true if the pseudocode will never stop, false otherwise.
 
 */
 
-function solution(a, b) 
+function isInfiniteProcess(a, b)
 {
+    let flag = a > b;
+    let cur = "";
+    let result = false;
+    let cnt = 0;
+
+    do
+    {
+        if (a == b)
+        {
+            return false;
+        }
+        
+        else if (flag != cur)
+        {
+            return true;
+        }
+
+        else
+        {
+            result = false;
+            a++;
+            b--;
+            cur = a > b;
+        }
+
+    } while (a != b);
+
+    return result;
 
 }
+
+function solution(a, b) 
+{
+    return isInfiniteProcess(a, b);
+}
+
+let a = 2;
+let b = 6;      // the output should be solution(a, b) = false;
+a = 2;
+b = 3;      // the output should be solution(a, b) = true.
+a = 3;
+b = 1;      // the output should be solution(a, b) = true.
+a = 10;
+b = 10;      // the output should be solution(a, b) = false
+
+console.log(isInfiniteProcess(a, b)); 
 
 /*
 
 ********
 BONEYARD
 ********
+
+
+
+        if (a == b)
+        {
+            return false;
+        }
+
+        else if (cnt == 20)
+        {
+            return true;
+        }
+
+        cnt++;
+
+    while (a != b)
+    {
+        if (a + 1 == b)
+        {
+            return false;
+        }
+
+        else if (a = b - 1)
+        {
+            return false;
+        }
+
+        else if (cnt == 20)
+        {
+            return false;
+        }
+
+        else
+        {
+            a++;
+            b--;
+            result = true;
+        }
+        
+    }
+
+// let result = true;
+    
+// console.log(a, b);
+                // a++;
+
+while a is not equal to b do
+  increase a by 1
+  decrease b by 1
+
 
 */
