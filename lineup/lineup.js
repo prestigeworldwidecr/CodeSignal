8af439b1dc924ea6cafcdd95b50dea4842f74e48
prestigeworldwidecr <prestigeworldwidecr@gmail.com>
2024-04-02 23:59:49 -0600

    lineup ex28

--- lineup/lineup.js
+++ lineup/lineup.js
@@ -34,15 +34,131 @@ The number of commands after which students face the same direction.
 
 */
 
+// starting position    move    end
+// front 1              
+//                      L       2
+//                      R       3
+//                      A       4
+// left 2
+//                      L       4
+//                      R       1
+//                      A       3
+// right 3
+//                      L       1
+//                      R       4
+//                      A       2
+// back 4
+//                      L       3
+//                      R       2
+//                      A       1
+
+function facingSame(a)
+{
+    let result = false;
+    
+    for (let i = 0; i < a.length - 1; i++)
+    {
+        if (a [i] == a [i + 1])
+        {
+            result = true;
+        }
+
+        else
+        {
+            return false;
+        }
+    }
+
+    return result;
+}
+
+function endState(start, move, movelist)
+{
+    let end = "";
+    
+    for (let i = 0; i < movelist.length; i++)
+    {
+        if (start == movelist[i][0])
+        {
+            if (move == movelist[i][1])
+            {
+                return movelist[i][2];
+            }
+
+            else
+            {
+
+            }
+
+        }
+
+        else
+        {
+            //
+        }
+
+    }
+
+}
+
+function teamMove(commands, team, movelist)
+{
+    let s = "";
+
+    for (let i = 0; i < commands.length; i++)
+    {
+        for (let j = 0; j < team.length; j++)
+        {
+            team[j] = endState(team[j], commands[i], movelist);
+        }
+    }
+
+    console.log(team);
+
+}
+
 function solution(commands) 
 {
 
 }
+
+let movelist = [[1, "L", 2],
+                [1, "R", 3],
+                [1, "A", 4],                
+                [2, "L", 4],            
+                [2, "R", 1],
+                [2, "A", 3],
+                [3, "L", 1],
+                [3, "R", 4],                
+                [3, "A", 2],            
+                [4, "L", 3],
+                [4, "R", 2],
+                [4, "A", 1]];
+
+let commands = "LLARL";     // the output should be solution(commands) = 3.
+let team = [1, 1, 2, 1];
+
+// console.log(facingSame(team));
+teamMove(commands, team, movelist);
 
 /*
 
 ********
 BONEYARD
 ********
+
+// console.log(team[i]);
+        // lets do one move, easy... start 1, end 2
+    // let teamMember1 = 1;
+    // let move = "L";
+
+    // endState(teamMember1, move, movelist);
+
+    
+// console.log(a [i], a [i + 1]);
+
+                // console.log("!");
+                // console.log("!");
+                // console.log(movelist[i][2]);
 
 */
