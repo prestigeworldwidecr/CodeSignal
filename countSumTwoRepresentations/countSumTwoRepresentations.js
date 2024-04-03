89ecd79f1d59da0aa4b31f863ade31460d767648
prestigeworldwidecr <prestigeworldwidecr@gmail.com>
2024-04-02 22:33:29 -0600

    countSumTwoRepresentations ex26

--- countSumTwoRepresentations/countSumTwoRepresentations.js
+++ countSumTwoRepresentations/countSumTwoRepresentations.js
@@ -41,10 +41,72 @@ r - l ≤ 106.
 
 */
 
-function solution(n, l, r) 
+function createInclusiveArray(l, r)
 {
+    let a = Array();
+
+    for (let i = l; i <= r; i++)
+    {
+        a.push(i);
+    }
+
+    return a;
+}
+
+function countSumTwoRepresentations(a, n)
+{
+    let b = Array();
+
+    for (let i = 0; i < a.length; i++)
+    {
+        let tmp = n - a [i];
+        
+        if (a.includes(tmp))
+        {
+            let tmp1 = "";
+            
+            if (a [i] < tmp)
+            {
+                tmp1 = a [i] + "" + tmp;
+            }
+
+            else
+            {
+                tmp1 = tmp + "" + a [i];
+            }
+            
+            // console.log(tmp1);
+            b.push(tmp1);
+
+        }
+
+        else
+        {
+            // 
+        }
+
+    }
+
+    result = [...new Set(b)];
+    // console.log(result);
+    return result.length;
 
 }
+
+
+function solution(n, l, r) 
+{
+    return countSumTwoRepresentations(createInclusiveArray(l, r), n);
+}
+
+let n = 6;
+let l = 2;
+let r = 4;      // the output should be solution(n, l, r) = 2.
+// n = 93;
+// l = 24;
+// r = 58;
+
+// countSumTwoRepresentations(createInclusiveArray(l, r), n);
 
 
 /*
@@ -52,5 +114,79 @@ function solution(n, l, r)
 ********
 BONEYARD
 ********
+
+// console.log(createInclusiveArray(l, r).includes(24));
+// test();
+
+// console.log("!");
+    // console.log(a);
+// let result = "";
+            // tmp1.sort();
+            // ]).toString();
+            // if (!b.includes(tmp1))
+            // {
+                // b.push(tmp1);
+            // }
+            
+            // else
+            // {
+                //
+            // }
+        // console.log (tmp1);
+
+function test()
+{
+    tmp = [[2, 4], [3, 3], [4, 2]];
+    
+    uniq = [...new Set(tmp)];
+
+    console.log(uniq);
+}
+
+function createInclusiveArray(l, r)
+{
+    // console.log("!");
+    let a = Array();
+
+    for (let i = l; i <= r; i++)
+    {
+        a.push(i);
+    }
+
+    // console.log(a);
+    return a;
+}
+
+function countSumTwoRepresentations(a, n)
+{
+    let b = [...a];
+    let cnt = 0;
+
+    b.reverse();
+
+    console.log(a, b);
+
+    for (let i = 0; i < Math.ceil(a.length / 2); i++)
+    {
+        console.log("!");
+        
+        let sum = a [i] + b [i];
+
+        if (sum == n)
+        {
+            console.log(a [i], b [i], n);
+            cnt++;
+        }
+
+        else
+        {
+            //
+        }
+    }
+
+    console.log(cnt);
+    
+    return cnt;
+}
 
 */
