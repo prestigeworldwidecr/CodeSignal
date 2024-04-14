6c67d4541b8bb8c28f8026387144c63af847a9cd
prestigeworldwidecr <prestigeworldwidecr@gmail.com>
2024-04-14 09:38:45 -0600

    candles ex33

--- candles/candles.js
+++ candles/candles.js
@@ -20,17 +20,94 @@ create a candle from the remaining leftovers;
 burn the last candle.
 Thus, you can burn 5 + 2 + 1 + 1 = 9 candles, which is the answer.
 
+Input/Output
+
+[execution time limit] 4 seconds (js)
+
+[memory limit] 1 GB
+
+[input] integer candlesNumber
+
+The number of candles you have in your possession.
+
+Guaranteed constraints:
+1 ≤ candlesNumber ≤ 15.
+
+[input] integer makeNew
+
+The number of leftovers that you can use up to create a new candle.
+
+Guaranteed constraints:
+2 ≤ makeNew ≤ 5.
+
+[output] integer
+
 */
+
+function makeCandles(makeNew, cnt, candlesNumber, leftovers)
+{    
+    // first candle
+    if (cnt == 0 && candlesNumber == 1)
+    {
+        console.log("fc", makeNew, cnt, candlesNumber, leftovers);
+        return 1;
+    }
+    
+    // burn candle
+    else if (candlesNumber > 1)
+    {
+        cnt = cnt + candlesNumber;
+        leftovers = leftovers + candlesNumber;
+        candlesNumber = 0;
+        console.log("bc", makeNew, cnt, candlesNumber, leftovers);
+        return makeCandles(makeNew, cnt, candlesNumber, leftovers);
+    }
+
+    // last candle
+    else if (candlesNumber == 1 && leftovers <= 1) // (Math.floor(leftovers / makeNew) > 0) )  
+    {
+        cnt = cnt + candlesNumber;
+        console.log("lc", makeNew, cnt, candlesNumber, leftovers);
+        return cnt;
+    }
+
+    // create candle
+    else if (leftovers > 1)
+    {
+        candlesNumber = Math.floor(leftovers / makeNew);     //
+        leftovers = candlesNumber % makeNew;                // 
+        console.log("cc", makeNew, cnt, candlesNumber, leftovers);
+        return makeCandles(makeNew, cnt, candlesNumber, leftovers);
+    }
+    
+    else
+    {
+        console.log("?c", makeNew, cnt, candlesNumber, leftovers);
+        return cnt + 1;
+    }
+    
+}
 
 function solution(candlesNumber, makeNew) 
 {
-
+    return makeCandles(makeNew, 0, candlesNumber, 0);
 }
+
+let candlesNumber = 5;
+let makeNew = 2;        // the output should be solution(candlesNumber, makeNew) = 9
+// candlesNumber = 1;
+// makeNew = 2;        // the output should be solution(candlesNumber, makeNew) = 1
+// candlesNumber = 3;
+// makeNew = 3;        // the output should be solution(candlesNumber, makeNew) = 4
+
+console.log("candle: ", makeCandles(makeNew, 0, candlesNumber, 0));
 
 /*
 
 ********
 BONEYARD
 ********
+
+// console.log(makeNew, cnt, candlesNumber, leftovers);
 
 */
