e7f8a45f8bad7b9ebf6a6fc63255364e5c047509
prestigeworldwidecr <prestigeworldwidecr@gmail.com>
2024-05-08 03:39:19 -0600

    add.py ex1

---
+++ add/add.py
@@ -0,0 +1,51 @@
+"""
+
+Write a function that returns the sum of two numbers.
+
+Example
+
+For param1 = 1 and param2 = 2, the output should be
+solution(param1, param2) = 3.
+
+Input/Output
+
+[execution time limit] 4 seconds (py3)
+
+[memory limit] 1 GB
+
+[input] integer param1
+
+Guaranteed constraints:
+-1000 ≤ param1 ≤ 1000.
+
+[input] integer param2
+
+Guaranteed constraints:
+-1000 ≤ param2 ≤ 1000.
+
+[output] integer
+
+The sum of the two inputs.
+
+[Python 3] Syntax Tips
+
+"""
+
+def solution(param1, param2):
+    return param1 + param2
+
+print(solution(2, 3))
+
+"""
+
+########
+BONEYARD
+########
+
+# Prints help message to the console
+# Returns a string
+def helloWorld(name):
+    print("This prints to the console when you Run Tests")
+    return "Hello, " + name
+
+"""
