05c827a2065a89af07f776dbc44d3d962e2150a4
prestigeworldwidecr <prestigeworldwidecr@gmail.com>
2024-08-14 08:57:36 -0600

    common_character_count.py ex10

--- commonCharacterCount/common_character_count.py
+++ commonCharacterCount/common_character_count.py
@@ -33,16 +33,92 @@ Guaranteed constraints:
 
 """
 
-# sort, count w/dict, compare
+# sort
+# count w/dict, compare
 
-dic1 = {}
+def create_character_count_dict(s) :
+# {
+    d = {}
 
-print(dic1)
+    for i in range(len(s)) :
+    # {
+        if (s[i] in d) :
+        # {
+            d[s[i]] = d[s[i]] + 1
+        # }
+
+        else :
+        # {
+            d[s[i]] = 1
+        # }
+
+    # }
+
+    return sorted(d)
+
+# }
+
+def determine_common_character_count(s1, s2) :
+# {
+    None
+# }
+
+def solution(s1, s2) :
+# {
+
+    if (len(s1) >= len(s2)) :
+    # {
+        determine_common_character_count(s1, s2)
+    # }
+
+    else :
+    # {
+        determine_common_character_count(s2, s1)
+    # }
+# }
+
+s1 = "aabcc"
+s2 = "adcaa"
+
+solution (s1, s2)
+
 
 """
 
 ########
 BONEYARD
 ########
+
+
+# print(len(s1))
+# print(create_character_count_dict(s2))
+
+# print(s1, s2)
+# print(max(len(s1), len(s2)))
+
+# "nothing"
+# d.update(s[i] = s[i], )
+            
+# "nothing"
+# print(s[i])
+
+# print(s1, s2, dic1)
+# print(s1[0])
+
+# for i in range()
+
+# dic1 = {}
+# dic1 = dict()
+
+dic1 = dict({
+                "name": "pizza",
+                "size": 16,
+                "location": "Chapultepec"
+            })
+
+sandwich_type = ("Reuben", "Club", "Triple Play")
+food_type = ("sandwich")
+
+dic1 = dict.fromkeys(sandwich_type, food_type)
 
 """
