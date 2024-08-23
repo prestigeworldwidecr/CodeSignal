669278cfa28e95a36fe240118152a14e7698bc84
prestigeworldwidecr <prestigeworldwidecr@gmail.com>
2024-08-23 12:05:17 -0600

    common_character_count.py ex10

--- commonCharacterCount/common_character_count.py
+++ commonCharacterCount/common_character_count.py
@@ -67,6 +67,15 @@ print(solution(sorted(s1), sorted(s2)))
 
 """
 
+def solution(s1, s2):
+    count = 0
+    commons = set(s1) & set(s2)
+    for i in commons:
+        count += min(s1.count(i), s2.count(i))
+    return count
+        
+
+
 ########
 BONEYARD
 ########
