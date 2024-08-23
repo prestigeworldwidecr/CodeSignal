af2e9e8a5d2bd9614a0432c4bdf6f51520d5e584
prestigeworldwidecr <prestigeworldwidecr@gmail.com>
2024-08-23 12:06:44 -0600

    is_lucky.py ex11

---
+++ isLucky/is_lucky.py
@@ -0,0 +1,40 @@
+"""
+
+Ticket numbers usually consist of an even number of digits. A ticket number is considered lucky if the sum of the first half of the digits is equal to the sum of the second half.
+
+Given a ticket number n, determine if it's lucky or not.
+
+Example
+
+For n = 1230, the output should be
+solution(n) = true;
+For n = 239017, the output should be
+solution(n) = false.
+Input/Output
+
+[execution time limit] 4 seconds (js)
+
+[memory limit] 1 GB
+
+[input] integer n
+
+A ticket number represented as a positive integer with an even number of digits.
+
+Guaranteed constraints:
+10 ≤ n < 10^6.
+10 ≤ n < 999999
+2, 4 or 6 digits
+
+[output] boolean
+
+true if n is a lucky ticket number, false otherwise.
+
+"""
+
+"""
+
+********
+BONEYARD
+********
+
+"""
