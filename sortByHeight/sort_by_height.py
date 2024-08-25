487ae648ab79b0b1e998e11f07ce43e71eb4dcb4
prestigeworldwidecr <prestigeworldwidecr@gmail.com>
2024-08-25 04:29:33 -0600

    sort_by_height.py ex11

--- sortByHeight/sort_by_height.py
+++ sortByHeight/sort_by_height.py
@@ -27,22 +27,65 @@ Sorted array a with all the trees untouched.
 
 """
 
+import sys
+
 def solution(a) :
 # {
-    print(a)
+    start_ppl = -1
+    b = a.copy()
+    b = sorted(b)
+
+    for i in range(len(b)) :
+    # {
+        print(i)
+        
+        if (b[i] != -1) :
+        # {
+            start_ppl = i
+            break
+        # }
+
+        else :
+        # {
+            None
+        # }
+
+    # }
+
+    print(start_ppl)
 # }
 
 a = [-1, 150, 190, 170, -1, -1, 160, 180] # [-1, 150, 160, 170, -1, -1, 180, 190]
 a1 = [4, 2, 9, 11, 2, 16]
 a2 = [23, 54, -1, 43, 1, -1, -1, 77, -1, -1, -1, 3]
 
-solution(a2)
+solution(a)
 
 """
 
 ********
 BONEYARD
 ********
+
+
+# i = sys.maxsize # exit
+# print(i)
+
+https://www.geeksforgeeks.org/enumerate-in-python/
+
+# print("hi")
+# print(b[i], i)
+        
+
+# copy 1 remove trees, sort
+    # original array, replace non-minus 1s w/None
+    # place copy 1 in order into None's
+
+    # print(str(a).replace("-1", "None"))
+    # can use list functions
+    # https://www.w3schools.com/python/python_lists_remove.asp
+    # b.remove(-1)
+    # b[0] = 99
 
 // stringToIntArray(tmp);
     // tmp[i] = parseInt(a[i]);
