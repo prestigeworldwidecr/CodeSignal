0c3c0b6947be8da35dc1737a52856677170c3422
prestigeworldwidecr <prestigeworldwidecr@gmail.com>
2024-08-25 09:22:28 -0600

    addBorder/add_border.py ex15

---
+++ addBorder/add_border.py
@@ -0,0 +1,86 @@
+"""
+
+Given a rectangular matrix of characters, add a border of asterisks(*) to it.
+
+Example
+
+For
+
+picture = ["abc",
+           "ded"]
+the output should be
+
+solution(picture) = ["*****",
+                     "*abc*",
+                     "*ded*",
+                     "*****"]
+Input/Output
+
+[execution time limit] 4 seconds (js)
+
+[memory limit] 1 GB
+
+[input] array.string picture
+
+A non-empty array of non-empty equal-length strings.
+
+Guaranteed constraints:
+1 ≤ picture.length ≤ 100,
+1 ≤ picture[i].length ≤ 100.
+
+[output] array.string
+
+The same matrix of characters, framed with a border of asterisks of width 1
+
+"""
+
+def addBorder(picture)
+# {
+    i = 0
+    tmp = ""
+    bottomTopBorder = "**"
+    result = []
+    
+    for (i i < picture[0].length i++)
+    # {
+        bottomTopBorder = bottomTopBorder + "*"
+    # }
+
+    result.push(bottomTopBorder)
+
+    for (i = 0 i < picture.length i++)
+    # {
+        tmp = "*" + picture[i] + "*"
+        result.push(tmp)
+    # }
+
+    result.push(bottomTopBorder)
+    
+    return result
+# }
+
+def solution(picture) 
+# {
+    return addBorder(picture)
+# }
+
+"""
+
+********
+BONEYARD
+********
+
+picture = ["abc",
+            "ded"]
+
+addBorder(picture)
+
+// solution(picture) = ["*****",
+//                      "*abc*",
+//                      "*ded*",
+//                      "*****"]
+
+// console.log("i: ", picture[i])
+    // console.log(result)
+
+"""
