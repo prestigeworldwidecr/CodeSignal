02d30f5c2738b9699ad690632d3e1da635ea19ca
prestigeworldwidecr <prestigeworldwidecr@gmail.com>
2024-09-05 11:28:12 -0600

    first_digit.py ex35

---
+++ firstDigit/first_digit.py
@@ -0,0 +1,52 @@
+"""
+
+Find the leftmost digit that occurs in a given string.
+
+Example
+
+For inputString = "var_1__Int", the output should be
+solution(inputString) = '1';
+For inputString = "q2q-q", the output should be
+solution(inputString) = '2';
+For inputString = "0ss", the output should be
+solution(inputString) = '0'.
+
+Input/Output
+
+[execution time limit] 4 seconds (js)
+
+[memory limit] 1 GB
+
+[input] string inputString
+
+A string containing at least one digit.
+
+Guaranteed constraints:
+3 ≤ inputString.length ≤ 10.
+
+[output] char
+
+"""
+
+def solution(inputString) :
+# {
+    # print(inputString)
+    return inputString
+# }
+
+# For inputString = "var_1__Int", the output should be solution(inputString) = '1';
+# For inputString = "q2q-q", the output should be solution(inputString) = '2';
+# For inputString = "0ss", the output should be solution(inputString) = '0'.
+inputString = "var_1__Int"
+inputString = "q2q-q"
+inputString = "0ss"
+print(solution(inputString))
+
+"""
+
+
+********
+BONEYARD
+********
+
+"""
