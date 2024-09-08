c98446b3c5738fa4e4ec7b25ebb462e421217835
prestigeworldwidecr <prestigeworldwidecr@gmail.com>
2024-09-08 05:12:15 -0600

    is_digit.py ex48

---
+++ isDigit/is_digit.py
@@ -0,0 +1,45 @@
+"""
+Determine if the given character is a digit or not.
+
+Example
+
+For symbol = '0', the output should be
+solution(symbol) = true;
+For symbol = '-', the output should be
+solution(symbol) = false.
+Input/Output
+
+[execution time limit] 4 seconds (js)
+
+[memory limit] 1 GB
+
+[input] char symbol
+
+A character which is either a digit or not.
+
+Guaranteed constraints:
+Given symbol is from ASCII table.
+
+[output] boolean
+
+true if symbol is a digit, false otherwise.
+
+"""
+
+def solution(symbol) :
+# {
+    return symbol.isdigit()
+# }
+
+# For symbol = '0', the output should be solution(symbol) = true;
+# For symbol = '-', the output should be solution(symbol) = false.
+
+
+
+"""
+
+********
+BONEYARD
+********
+
+"""
