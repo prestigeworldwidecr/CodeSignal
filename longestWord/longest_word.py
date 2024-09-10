8832390aaf8167da2b147b95a70bbce19747e808
prestigeworldwidecr <prestigeworldwidecr@gmail.com>
2024-09-10 05:40:03 -0600

    longest_word.py ex52

---
+++ longestWord/longest_word.py
@@ -0,0 +1,45 @@
+"""
+
+Define a word as a sequence of consecutive English letters. Find the longest word from the given string.
+
+Example
+
+For text = "Ready, steady, go!", the output should be
+solution(text) = "steady".
+
+Input/Output
+
+[execution time limit] 4 seconds (js)
+
+[memory limit] 1 GB
+
+[input] string text
+
+Guaranteed constraints:
+4 ≤ text.length ≤ 50.
+
+[output] string
+
+The longest word from text. It's guaranteed that there is a unique output.
+
+"""
+
+def longest_word(text) :
+# {
+    None
+# }
+
+def solution(text) :
+# {
+    return longest_word(text)
+# }
+
+text = "Ready, steady, go!";    # the output should be solution(text) = "steady".
+
+"""
+
+********
+BONEYARD
+********
+
+"""
