577e1a49b4a65e734ca995cd731f12dd3e3cdfad
prestigeworldwidecr <prestigeworldwidecr@gmail.com>
2024-09-11 02:01:04 -0600

    digits_product.py ex56

---
+++ digitsProduct/digits_product.py
@@ -0,0 +1,32 @@
+"""
+
+Given an integer product, find the smallest positive (i.e. greater than 0) integer the product of whose digits is equal to product. If there is no such integer, return -1 instead.
+
+Example
+
+For product = 12, the output should be
+solution(product) = 26;
+For product = 19, the output should be
+solution(product) = -1.
+Input/Output
+
+[execution time limit] 4 seconds (js)
+
+[memory limit] 1 GB
+
+[input] integer product
+
+Guaranteed constraints:
+0 ≤ product ≤ 600.
+
+[output] integer
+
+"""
+
+"""
+
+********
+BONEYARD
+********
+
+"""
