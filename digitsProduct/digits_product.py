a5cf04bb9d2348f7356ae6ddadcc5d25270a729b
prestigeworldwidecr <prestigeworldwidecr@gmail.com>
2024-09-11 03:26:26 -0600

    digits_product.py ex56

--- digitsProduct/digits_product.py
+++ digitsProduct/digits_product.py
@@ -23,10 +23,70 @@ Guaranteed constraints:
 
 """
 
+import math
+import sys
+
+# determine all multiples
+# find min
+
+def solution(product) :
+# {
+    min = sys.maxsize
+
+    for i in range(2, math.ceil(product / 2)) :
+    # {
+        if ((product / i) % 1 == 0) :
+        # {
+            tmp = int(str(i) + str(int(product / i)))
+            
+            if (tmp < min) :
+            # {
+                min = tmp
+            # }
+
+            else :
+            # {
+                None
+            # }
+
+        # }
+
+        else :
+        # {
+            None
+        # }
+
+    # }
+
+    if (min == sys.maxsize) :
+    # {
+        min = -1
+    # }
+
+    else :
+    # {
+        None
+    # }
+
+    return min
+
+# }
+
+# For product = 12, the output should be solution(product) = 26;
+# For product = 19, the output should be solution(product) = -1.
+product = 12
+# product = 19
+
+print(solution(product))
+
 """
 
 ********
 BONEYARD
 ********
+
+# print(math.ceil(product / 2))
+            # print('!', product / i)
+            # print('!', i, int(product / i))
 
 """
