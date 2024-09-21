8bdb7c6756822dc71759556d9d5db11742706236
prestigeworldwidecr <prestigeworldwidecr@gmail.com>
2024-09-21 06:33:59 -0600

    circle_of_numbers.py ex6

--- circleOfNumbers/circle_of_numbers.py
+++ circleOfNumbers/circle_of_numbers.py
@@ -9,6 +9,8 @@ Example
 For n = 10 and firstNumber = 2, the output should be
 solution(n, firstNumber) = 7.
 
+
+
 Input/Output
 
 [execution time limit] 4 seconds (js)
@@ -31,46 +33,15 @@ Guaranteed constraints:
 
 """
 
-def circle_of_numbers(n, first_number) :
-# {
-    diameter = n / 2
-    result = None
 
-    if (first_number < diameter):
-    # {
-        result = first_number + diameter
-    # }
 
-    elif (first_number >= diameter) :
-    # {
-        result = first_number - diameter
-    # }
-
-    else :
-    # {
-        None
-    # }
-
-    return result
-
-# }
-
-def solution(n, first_number) :
-# {
-    return circle_of_numbers(n, first_number)
-# }
-
-# For n = 10 and firstNumber = 2, the output should be solution(n, firstNumber) = 7.
 n = 10
 first_number = 2
-
-# print(solution(n, first_number))
 
 """
 
 ********
 BONEYARD
 ********
-
 
 """
