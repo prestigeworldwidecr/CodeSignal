b43805f55e9c162c18315da5e56bffcbd094a0be
prestigeworldwidecr <prestigeworldwidecr@gmail.com>
2024-10-19 03:29:17 -0600

    practice1.py

---
+++ course/unit1/practice1.py
@@ -0,0 +1,33 @@
+'''
+
+Welcome to the first practice of this course! Let's start by making sure you know enough Python to progress through the rest of the course successfully! Fill in the missing chunk of the code to make sure this code runs correctly. 🚀
+
+'''
+
+def print_dict_elements(dct):
+    for key, value in dct.items():
+        print(key, ':', value)
+
+# Dictionary containing information about rockets
+rocket_info = {
+    "Falcon 1": "First privately developed liquid-fueled rocket",
+    "Atlas V": "Launch vehicle for Mars Rovers",
+    "Saturn V": "Rocket that took humans to the Moon",
+    "Space Shuttle": "First reusable spacecraft"
+}
+
+print_dict_elements(rocket_info)
+
+# Expected Output:
+# Falcon 1: First privately developed liquid-fueled rocket
+# Atlas V: Launch vehicle for Mars Rovers
+# Saturn V: Rocket that took humans to the Moon
+# Space Shuttle: First reusable spacecraft
+
+'''
+
+********
+BONEYARD
+********
+
+'''
