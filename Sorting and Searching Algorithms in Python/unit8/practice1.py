bee934602ed480a4c522524ade5618c090ea7329
prestigeworldwidecr <prestigeworldwidecr@gmail.com>
2024-12-22 05:38:22 -0600

    practice1.py

--- Sorting and Searching Algorithms in Python/unit8/practice1.py
+++ Sorting and Searching Algorithms in Python/unit8/practice1.py
@@ -30,9 +30,27 @@ def find_kth_largest(numbers, k) :
 # {
     if (numbers) :
     # {
-        # implement this
-        None
-    # }
+        pos = partition(numbers, 0, len(numbers) - 1)
+    
+        if (k - 1 == pos) :
+        # {
+            # The pivot is the k-th element after partitioning
+            return numbers[pos]
+        # }
+
+        elif (k - 1 > pos) :
+        # {
+            # The pivot index after partitioning is larger than k
+            # We'll keep searching in the left part
+            return find_kth_largest(numbers[ : pos: ], k)
+        # }
+
+        else :
+        # {
+            # The pivot index after partitioning is smaller than k
+            # We'll keep searching in the right part
+            return find_kth_largest(numbers[pos + 1: : ], k - pos - 1)
+        # }
 
     else :
     # {
@@ -43,8 +61,41 @@ def find_kth_largest(numbers, k) :
         
 def partition(nums, l, r) :
 # {
-    # implement this
-    None
+    # Choosing a random index to make the algorithm less deterministic
+    rand_index = random.randint(l, r)
+
+    # nums[l], nums[rand_index] = nums[rand_index], nums[l]
+    tmp = nums[l]
+    nums[l] = nums[rand_index]
+    nums[rand_index] = tmp
+    pivot_index = l
+
+    for i in range(l + 1, r + 1) :
+    # {
+        if (nums[i] <= nums[l]) :
+        # {
+            # pivot_index += 1
+            pivot_index = pivot_index + 1
+            # nums[i], nums[pivot_index] = nums[pivot_index], nums[i]
+            tmp = nums[i]
+            nums[i] = nums[pivot_index]
+            nums[pivot_index] = tmp
+        # }
+
+        else :
+        # {
+            None
+        # }
+
+    # }
+
+    # nums[pivot_index], nums[l] = nums[l], nums[pivot_index]
+    tmp = nums[pivot_index]
+    nums[pivot_index] = nums[l]
+    nums[l] = tmp
+
+    return pivot_index
+
 # }
 
 print(find_kth_largest([3, 2, 1, 5, 6, 4], 2))  # Expected output: 5
