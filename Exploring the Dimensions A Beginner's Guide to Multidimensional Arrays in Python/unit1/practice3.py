5a36bbf27680df9486ac1adbe681e0b49fa267ff
prestigeworldwidecr <prestigeworldwidecr@gmail.com>
2025-07-31 19:58:01 -0600

    practice3.py

---
+++ Exploring the Dimensions A Beginner's Guide to Multidimensional Arrays in Python/unit1/practice3.py
@@ -0,0 +1,22 @@
+'''
+
+Great job so far! Now, let's challenge you a bit more. Your task is to print the codes of the apartments on the first floor of our building. Here's a tiny challenge: Fill in the missing part to complete your task with minimal change. Remember, simplicity is key!
+
+'''
+
+# Creating a 2D array representing an apartment building
+building = [["1A", "1B", "1C"], 
+            ["2A", "2B", "2C"],
+            ["3A", "3B", "3C"]]
+
+# TODO: Complete the code to print the apartment codes of the first floor only.
+for apartment in building[0] :
+# {
+    print(apartment, ' ')
+# }
+
+'''
+
+***** BONEYARD *****
+
+'''
