200a1dd13443939aa865133fac97a1d152fa7b70
prestigeworldwidecr <prestigeworldwidecr@gmail.com>
2025-08-07 05:37:27 -0600

    practice4.py

--- Exploring the Dimensions A Beginner's Guide to Multidimensional Arrays in Python/unit4/practice4.py
+++ Exploring the Dimensions A Beginner's Guide to Multidimensional Arrays in Python/unit4/practice4.py
@@ -2,46 +2,58 @@
 
 Great job navigating the board so far, young Space Voyager! Now, let's challenge you a bit more. There's a magical spell (also known as "code") that allows you to inspect the chessboard for wise moves. Can you figure out the incantation (or code) that helps identify possible locations for a new piece, ensuring it has room to move in the next turn? Focus on checking the conditions for a spot to be deemed valid.
 
+Good effort, but your function isn't quite checking the right conditions yet.
+
+Make sure you:
+
+Check if the current cell is empty before looking at neighbors.
+Carefully check the correct neighbors (up, down, left, right) using the right indices.
+Use a list comprehension to collect all valid positions.
+Want to give it another shot or need a hint?
+
+Cosmo
+Yesterday
+Read message aloud
+Good effort, but your function has some boundary issues and you haven't used a list comprehension to print all valid move positions yet.
+
+Want to try fixing those parts? Let me know if you need a hint!
+
+Cosmo
+Yesterday
+Read message aloud
+Good effort, but your function should return True or False, not a list. Also, you need to use a list comprehension to find all valid move positions on the board.
+
+Want to try fixing those parts? Let me know if you need a hint!
+
+Cosmo
+2:21 AM
+Read message aloud
+Good effort, but your function isn't checking all four neighbors or if the cell is empty first. Also, try to return the list of valid positions instead of just printing True/False.
+
+Want to give it another shot or need a hint?
+
 '''
 
 def evaluate_move(board, row, col) :
 # {
-    result = False
+    result = []
     
     # TODO: Check if a move to the given cell is possible; write a condition to check if the cell is empty.
     # Also, check if at least one neighboring cell is empty (not diagonally).
-    if (row > 0) :
+    if (board[row][col] == 'E') :
     # {
-        if (board[row - 1][col] == 'E') :
+        if (row > 0) :
         # {
-            return True
-        # }
+            if (board[row - 1][col] == 'E') :
+            # {
+                result.append((row - 1, col))
+            # }
 
-        else :
-        # {
-            None
-        # }
-        
-    # }
-
-    elif (row < total_rows) :
-    # {
-        if (board[row + 1][col] == 'E') :
-        # {
-            return True
-        # }
-
-        else :
-        # {
-            None
-        # }
-    # }
-
-    elif (col > 0) :
-    # {
-        if (board[row][col - 1] == 'E') :
-        # {
-            return True
+            else :
+            # {
+                None
+            # }
+            
         # }
 
         else :
@@ -49,23 +61,70 @@ def evaluate_move(board, row, col) :
             None
         # }
 
-    # }
-
-    elif (col < total_columns) :
-    # {
-        if (board[row][col + 1] == 'E') :
+        if (row < total_rows - 1) :
         # {
-            return True
+            # print(row, col)
+
+            if (board[row + 1][col] == 'E') :
+            # {
+                result.append((row + 1, col))
+            # }
+
+            else :
+            # {
+                None
+            # }
+
         # }
 
         else :
         # {
             None
         # }
-        
+
+        if (col > 0) :
+        # {
+            if (board[row][col - 1] == 'E') :
+            # {
+                result.append((row, col - 1))
+            # }
+
+            else :
+            # {
+                None
+            # }
+
+        # }
+
+        else :
+        # {
+            None
+        # }
+
+        if (col < total_columns - 1) :
+        # {
+            # print(i, j, row, col)
+            
+            if (board[row][col + 1] == 'E') :
+            # {
+                result.append((row, col + 1))
+            # }
+
+            else :
+            # {
+                None
+            # }
+
+        # }
+
+        else :
+        # {
+            None
+        # }
+
     # }
 
-    else :
+    else : 
     # {
         None
     # }
@@ -113,8 +172,6 @@ for i in range(len(valid_moves)) :
 '''
 
 ***** BONEYARD *****
-
-
 
     # }
 
