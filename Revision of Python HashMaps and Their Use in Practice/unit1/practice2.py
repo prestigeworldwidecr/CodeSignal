6b34b9928f4f89eabb5cfaa3e77a5efb3efb31db
prestigeworldwidecr <prestigeworldwidecr@gmail.com>
2025-08-17 03:19:15 -0600

    practice2.py

---
+++ Revision of Python HashMaps and Their Use in Practice/unit1/practice2.py
@@ -0,0 +1,21 @@
+'''
+
+Great job on understanding dictionaries, Space Explorer! Now, let's further test your skills. Here's a piece of code that intends to print the title of a specific book from a library catalog, but it seems to miss the mark. Can you spot the issue and make the necessary fix to ensure it outputs the correct book title? Dive in and show what you've learned!
+
+'''
+
+# Simple HashMap example to catalog books and print a specific book title
+library_catalog = {
+                    "book1" : "The Little Prince",
+                    "book2" : "The Hobbit",
+                    "book3" : "The Catcher in the Rye"
+                }
+
+# Access and print the title of book2 from the catalog
+print(library_catalog["book2"])
+
+'''
+
+***** BONEYARD *****
+
+'''
