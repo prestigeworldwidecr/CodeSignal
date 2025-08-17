f0d5d73c0b6f5e106ad55acede83ef2a83ac3a8f
prestigeworldwidecr <prestigeworldwidecr@gmail.com>
2025-08-17 10:05:19 -0600

    practice5.py

---
+++ Revision of Python HashMaps and Their Use in Practice/unit1/practice5.py
@@ -0,0 +1,22 @@
+'''
+
+You're on the final step of understanding HashMaps using Python dictionaries. Dive into creating a simple library catalog: update book titles and print the updated catalog. Start from scratch and craft each part carefully.
+
+'''
+
+# TODO: Create a dictionary named library_catalog with 'book1' and 'book2' as keys and their titles as values
+library_catalog = {"book1" : "Parker Lewis Can't Lose",
+           "book2" : "Firefly"
+           }
+
+# TODO: Update the title for 'book1' in the dictionary
+library_catalog["book1"] = "Bad News Bears"
+
+# TODO: Print the updated title for 'book1'
+print(library_catalog["book1"])
+
+'''
+
+***** BONEYARD *****
+
+'''
