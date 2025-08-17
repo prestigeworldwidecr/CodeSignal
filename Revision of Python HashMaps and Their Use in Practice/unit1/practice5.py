54823a68a207bf5fd71f9c5ce0ff607b54918ee3
prestigeworldwidecr <prestigeworldwidecr@gmail.com>
2025-08-17 12:38:54 -0600

    practice5.py

--- Revision of Python HashMaps and Their Use in Practice/unit1/practice5.py
+++ Revision of Python HashMaps and Their Use in Practice/unit1/practice5.py
@@ -5,9 +5,9 @@ You're on the final step of understanding HashMaps using Python dictionaries. Di
 '''
 
 # TODO: Create a dictionary named library_catalog with 'book1' and 'book2' as keys and their titles as values
-library_catalog = {"book1" : "Parker Lewis Can't Lose",
-           "book2" : "Firefly"
-           }
+library_catalog = { "book1" : "Parker Lewis Can't Lose",
+                    "book2" : "Firefly"
+                    }
 
 # TODO: Update the title for 'book1' in the dictionary
 library_catalog["book1"] = "Bad News Bears"
